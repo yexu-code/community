c408ae65a8cef8f6b1fd66b223e4e2c9c89e072d
lilei <yx163lilei@163.com>
2022-05-24 11:00:07 +0800

    2022.5.24提交

--- src/main/java/com/nowcoder/community/controller/MessageController.java
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -5,6 +5,7 @@ import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.MessageService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class MessageController {
@@ -33,6 +32,8 @@ public class MessageController {
     // 私信列表
     @RequestMapping(path = "/letter/list", method = RequestMethod.GET)
     public String getLetterList(Model model, Page page){
+        // Integer.parseInt("abc"); // 测试500错误
+
         User user = hostHolder.getUser();
         // 分页信息
         page.setLimit(5);
@@ -65,7 +66,7 @@ public class MessageController {
     }
 
     @RequestMapping(path = "/letter/detail/{conversationId}", method = RequestMethod.GET)
-    public String geLetterDetail(@PathVariable("conversationId") String conversationId, Page page, Model model){
+    public String getLetterDetail(@PathVariable("conversationId") String conversationId, Page page, Model model){
         // 分页信息
         page.setLimit(5);
         page.setPath("/letter/detail/" + conversationId);
@@ -87,6 +88,12 @@ public class MessageController {
         // 私信目标
         model.addAttribute("target", getLetterTarget(conversationId));
 
+        // 设置已读
+        List<Integer> ids = getLetterIds(letterList);
+        if (!ids.isEmpty()) {
+            messageService.readMessage(ids);
+        }
+
         return "/site/letter-detail";
 
     }
@@ -101,6 +108,46 @@ public class MessageController {
         } else {
             return userService.findUserById(id0);
         }
+    }
+
+    // 获取私信列表中的未读消息id
+    private List<Integer> getLetterIds(List<Message> letterList){
+        List<Integer> ids = new ArrayList<>();
+
+        if (letterList != null) {
+            for (Message message : letterList){
+                if (hostHolder.getUser().getId() == message.getToId() && message.getStatus() == 0) {
+                    ids.add(message.getId());
+                }
+            }
+        }
+
+        return ids;
+    }
+
+    @RequestMapping(path = "/letter/send", method = RequestMethod.POST)
+    @ResponseBody
+    public String sendLetter(String toName, String content){
+        // Integer.parseInt("abc"); // 测试异步请求异常500
+
+        User target = userService.findUserByName(toName);
+        if (target == null) {
+            return CommunityUtil.getJSONString(1, "目标用户不存在!");
+        }
+
+        Message message = new Message();
+        message.setFromId(hostHolder.getUser().getId());
+        message.setToId(target.getId());
+        if (message.getFromId() < message.getToId()){
+            message.setConversationId(message.getFromId() + "_" + message.getToId());
+        } else {
+            message.setConversationId(message.getToId() + "_" + message.getFromId());
+        }
+        message.setContent(content);
+        message.setCreateTime(new Date());
+        messageService.addMessage(message);
+
+        return CommunityUtil.getJSONString(0);
     }
 
 }
--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -174,4 +174,7 @@ public class UserService implements CommunityConstant {
         return userMapper.updatePassword(userId, password);
     }
 
+    public User findUserByName(String username){
+        return userMapper.selectByName(username);
+    }
 }
